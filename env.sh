e999e57fab42a5112a0987f20e6be049b4ed4e83
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-12-10 14:45:59 +0000

    add Jupyter notebook examples

---
+++ env.sh
@@ -0,0 +1,5 @@
+module use /scratch1/NCEPDEV/da/Cory.R.Martin/Modulefiles/python/
+module load anaconda/kevin
+module load emcpy/0.1
+export PYTHONPATH=/scratch1/NCEPDEV/da/Kevin.Dougherty/emcpy/src/:$PYTHONPATH
+export PYTHONPATH=/scratch1/NCEPDEV/da/Kevin.Dougherty/PyGSI/:$PYTHONPATH
